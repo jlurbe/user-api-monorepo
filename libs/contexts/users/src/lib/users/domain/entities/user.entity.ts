b911f35b5af8fbb791fb4aa92b024cdefbd47d16
JuanJo Lurbe <juanjo.lurbe@gmail.com>
2025-04-14 21:44:58 +0200

    feat(users): include user creation from primitives amd from user command in user entity

--- libs/contexts/users/src/lib/users/domain/entities/user.entity.ts
+++ libs/contexts/users/src/lib/users/domain/entities/user.entity.ts
@@ -4,6 +4,8 @@ import { UserIdValueObject } from '../value-objects/user-id.value-object';
 import { UserNameValueObject } from '../value-objects/user-name.value-object';
 import { UserEmailValueObject } from '../value-objects/user-email.value-object';
 import { UserPasswordValueObject } from '../value-objects/user-password.value-object';
+import { CreateUserCommand } from '../../application/commands/create-user.command';
+import { v4 as uuid } from 'uuid';
 
 export class UserEntity extends Entity<UserEntity, UserPrimitive> {
   readonly id: UserIdValueObject;
@@ -36,5 +38,27 @@ export class UserEntity extends Entity<UserEntity, UserPrimitive> {
       email: this.email._value,
       password: this.password._value,
     };
+  }
+
+  static fromPrimitives(userPrimitive: UserPrimitive): UserEntity {
+    return new UserEntity({
+      id: new UserIdValueObject(userPrimitive.id),
+      name: new UserNameValueObject(userPrimitive.name),
+      email: new UserEmailValueObject(userPrimitive.email),
+      password: new UserPasswordValueObject(userPrimitive.password),
+    });
+  }
+
+  static async fromCreateUserCommand(
+    createUserCommand: CreateUserCommand,
+  ): Promise<UserEntity> {
+    return new UserEntity({
+      id: new UserIdValueObject(uuid()),
+      name: new UserNameValueObject(createUserCommand.name),
+      email: new UserEmailValueObject(createUserCommand.email),
+      password: await UserPasswordValueObject.create(
+        createUserCommand.password,
+      ),
+    });
   }
 }
